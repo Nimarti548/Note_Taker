a1d9a9f8c1b1dee2f9c6059b160c0eff320b58a5
Nicolas Martinez <Nimarti548@gmail.com>
2020-12-16 13:58:18 -0800

    UPDATE: created api and client routes folders and added get request

---
+++ routes/api-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { nanoid } = require("nanoid")
+const fs = require('fs')
+const path = require("path");
+const db = path.join(__dirname, "../db/db.json");
+
+router.get("/notes", (req, res) => {
+    fs.readFile(db, "utf8", (err, data) => {
+        if (err) throw err;
+        return res.json(JSON.parse(data));
+    });
+});
+
+
+
+ 
+
+module.exports = router
