0a6c345e742a6945c1d0d9eb5dc3db8d850d067b
Nicolas Martinez <Nimarti548@gmail.com>
2020-12-26 14:29:22 -0800

    UPDATE: fixed the delete method now able to delete notes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,7 +13,7 @@ router.post("/notes", (req, res) => {
     .catch(err => res.status(500).json(err))
 })  
 
-router.delete("/notes", (req,res) => {
+router.delete("/notes/:id", (req,res) => {
     note.deleteNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch(err => res.status(500).json(err))
